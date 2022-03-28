e03a0a4313c73a8aacc275d6b8aafd6b356b6157
RayhanHamada <rayhanhamada1999@gmail.com>
2022-03-28 14:34:36 +0700

    chore: init and do stuffs

---
+++ src/tasks/getNewListing.ts
@@ -0,0 +1,39 @@
+import { load } from 'cheerio';
+import type { Page } from 'puppeteer';
+import dayjs from '../utils/dayjs';
+
+export default async function getNewListing(page: Page) {
+  await page.goto(
+    'https://www.binance.com/en/support/announcement/c-48?navId=48'
+  );
+
+  // get all list
+  const els = await page.$x(
+    '//*[@id="__APP"]/div/div/main/div[2]/div[2]/section/div[2]/div[1]'
+  );
+
+  // take only first level div
+  const el = await els[0].$$(':scope > div');
+
+  const htmlArr = await Promise.all(
+    el.map(async (e) => await e.evaluate((e) => e.innerHTML))
+  );
+
+  // extract title, link, and date
+  const extracts = htmlArr.map((e) => {
+    const loaded = load(e);
+
+    const link = `https://www.binance.com${loaded('a').attr('href')}`;
+    const title = (loaded('a > div').contents()[0] as any).data as string;
+    const textDate = loaded('a > div > h6').text();
+    const date = dayjs(textDate, 'YYYY-MM-DD');
+
+    return {
+      link,
+      title,
+      date,
+    };
+  });
+
+  console.log(extracts);
+}
---
+++ src/utils/dayjs.ts
@@ -0,0 +1,5 @@
+import dayjs from 'dayjs';
+
+// do your locale, plugin, or config here
+
+export default dayjs;
