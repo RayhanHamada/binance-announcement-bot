bf4e0e0fe60270858598a6e67f25fb770296e95b
RayhanHamada <rayhanhamada1999@gmail.com>
2022-04-16 01:26:43 +0700

    chore: add comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import bot from './bot';
 import { botToken } from './constants';
 
-bot.login(botToken).then(() => {
-  console.log(`start bot`);
-});
+/**
+ * trigger task for every hour at X:10
+ */
+// const executeTaskCron = parseCronExpression('0 10 * * * *');
+
+/**
+ * trigger collection reset every 23:50
+ */
+// const resetDBCron = parseCronExpression('0 50 23 * * *');
+
+bot.login(botToken).then(() => {});
