dcc9a34f55763bb11f6570a55fc8fdad836fc26f
RayhanHamada <rayhanhamada1999@gmail.com>
2022-04-18 02:49:40 +0700

    chore: add test

---
+++ src/scrapeTask/getNewCryptoListing.spec.ts
@@ -0,0 +1,23 @@
+import { launch } from 'puppeteer';
+import getNewCryptoListing from './getNewCryptoListing';
+
+describe('scrapeTask/getNewCryptoListing', () => {
+  jest.setTimeout(20000);
+  test('should work properly', async () => {
+    const browser = await launch();
+    const page = await browser.newPage();
+
+    const testFn = async () => await getNewCryptoListing(page);
+
+    expect(testFn).not.toThrowError();
+  });
+
+  test('should return array', async () => {
+    const browser = await launch();
+    const page = await browser.newPage();
+
+    const testFn = async () => await getNewCryptoListing(page);
+
+    expect(Array.isArray(await testFn())).toBeTruthy();
+  });
+});
